4a1d18b0e139868317203f507460918b16325e21
Inanc Yuce <inancyuce@Inancs-Mac-mini.local>
2013-11-29 01:41:36 +0200

    Initial Commit

---
+++ People Database/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  People Database
+//
+//  Created by Inanc Yuce on 29/11/13.
+//  Copyright (c) 2013 YUCE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
